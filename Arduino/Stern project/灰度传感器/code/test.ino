9ccfd2884af7cde9054261d48cc6960cf9a2f704
pengchihui1 <51830726+pengchihui1@users.noreply.github.com>
2020-08-07 23:26:56 +0800

    qw

--- Arduino/Stern project/灰度传感器/code/test.ino
+++ Arduino/Stern project/灰度传感器/code/test.ino
@@ -12,17 +12,17 @@ void loop(){
 }
 void compare(int before,int after){  //白过度到黑的过程 读取的值越来越小 顺时针  小-大=负数
      int dff=after-before;
-     if(dff<-5 && dff>-100){  //-5代表变化大于5  
+     if(dff<-5 && dff>-100){        //白过度到黑的过程  变化大于5时  
            Serial.print("顺");
            Serial.println(dff);
-       }else if(dff>5 && dff<100){    //5代表变化大于5
+       }else if(dff>5 && dff<100){    //黑过度到白  变化大于5时 
            Serial.print("逆");
            Serial.println(dff);
-       }else if(dff<-100){     //黑色跳白色
+       }else if(dff<-100){     //黑色跳白色 很快时
            Serial.print("逆");
            Serial.println(dff);
        }else if(dff>100){
-           Serial.print("顺");  //白色跳黑色
+           Serial.print("顺");  //白色跳黑色  很快时
            Serial.println(dff);
        }
 }
