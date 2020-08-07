80be137deba2a1bc3ea43564a710464c20e32e4b
pengchihui1 <51830726+pengchihui1@users.noreply.github.com>
2020-08-07 23:23:16 +0800

    Create test.ino

---
+++ Arduino/Stern project/灰度传感器/code/test.ino
@@ -0,0 +1,28 @@
+int LDR_pin = A5;   //定义灰度模拟口 A5
+void setup(){
+  Serial.begin(9600); //设置波特率9600
+}
+int previousGrayScale=0; //前一个值
+int GrayScale=0; //当前值
+void loop(){   
+       previousGrayScale=GrayScale;      //前一个值当前值
+       GrayScale=analogRead(LDR_pin);    //当前值为最新的值
+       compare(previousGrayScale,GrayScale);
+       delay(100); 
+}
+void compare(int before,int after){  //白过度到黑的过程 读取的值越来越小 顺时针  小-大=负数
+     int dff=after-before;
+     if(dff<-5 && dff>-100){  //-5代表变化大于5  
+           Serial.print("顺");
+           Serial.println(dff);
+       }else if(dff>5 && dff<100){    //5代表变化大于5
+           Serial.print("逆");
+           Serial.println(dff);
+       }else if(dff<-100){     //黑色跳白色
+           Serial.print("逆");
+           Serial.println(dff);
+       }else if(dff>100){
+           Serial.print("顺");  //白色跳黑色
+           Serial.println(dff);
+       }
+}
