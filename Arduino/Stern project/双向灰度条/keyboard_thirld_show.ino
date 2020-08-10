17e5ae315d1d12314b1383bfc6dd0f4996f8f652
pengchihui1 <pengchihui1@gmail.com>
2020-08-10 19:12:30 +0800

    灰度左右键盘程式码

--- Arduino/Stern project/双向灰度条/keyboard_thirld_show.ino
+++ Arduino/Stern project/双向灰度条/keyboard_thirld_show.ino
@@ -1,6 +1,6 @@
 #include <Keyboard.h>
-int pinA = A2;   //定义灰度模拟   A传感器
-int pinB = A4;   //定义灰度模拟   B传感器
+int pinA = A5;   //定义灰度模拟   外部传感器(A)
+int pinB = A4;   //定义灰度模拟   内部传感器(B)
 void setup(){
   Keyboard.begin(); //  初始化键盘控制:
   Serial.begin(9600); //设置波特率9600
@@ -15,40 +15,34 @@ void loop(){
        previousGrayScaleA=GrayScaleA;      //A传感器 前一个值当前值
        GrayScaleA=analogRead(pinA);        //A传感器 当前值为最新的值
 	   
-	   previousGrayScaleB=GrayScaleB;      //B传感器 前一个值当前值
-	   GrayScaleB=analogRead(pinB);        //B传感器 当前值为最新的值
+	    previousGrayScaleB=GrayScaleB;      //B传感器 前一个值当前值
+	    GrayScaleB=analogRead(pinB);        //B传感器 当前值为最新的值
 	   
        compare(previousGrayScaleA,GrayScaleA,previousGrayScaleB,GrayScaleB);
        delay(100); 
 }
 
 void compare(int beforeA,int nowA,int beforeB,int nowB){  
-	
-		 if(nowA>350 && nowA<450){           //A传感器在350-450   相信B
-		 
-			 contrast(beforeB,nowB);
-			 
-		 }else if(nowB>350 && nowA<nowB){    //B传感器在350-450   相信A
-		 
-			  contrast(beforeA,nowA);
-			  
-		 }else if(nowA>=450 && nowA<=550){    //A传感器在450-550  相信A
-			  
-			 contrast(beforeA,nowA);
-			 
-		 }else if(nowA>=250 && nowA<=350 ){   //A传感器在250-350  相信A
-		   
-		      contrast(beforeA,nowA);
-			 
-		 } 
+	   //黑过度到白
+		 if(nowA>=395 && nowA<=510){           //外部的传感器(A)在395-510   内部传感器在黑白中间线的两侧   信外部的传感器(A)
+		     contrast(beforeA,nowA);
+		 }else if(nowB>=395 && nowB<=510){    //内部的传感器(B)在395-510    外部传感器在黑白中间线的两侧   信外部的传感器(B)
+			 	 contrast(beforeB,nowB);
+		 }else if((nowA>200 && nowA<395)||(nowA>510 && nowA<750)){    //外部的传感器(A)在200-395 510-750    信外部的传感器(A)
+			   contrast(beforeA,nowA);
+		 }
 }
 
 void contrast(int prev,int after){
-	//黑过度到白
 	int dff=after-prev;
-	if(dff<-5){
-	        Keyboard.press(KEY_LEFT_ARROW);   //按下方向左鍵  逆方向
-	  }else if(dff>5 ){    
-		   Keyboard.press(KEY_RIGHT_ARROW);  //按下方向右鍵  顺方向 
+	if(dff>5 ){    
+		    Serial.println("顺");
+				Keyboard.println("A");
+				// Keyboard.println(KEY_RIGHT_ARROW);  //右键  
+	  }else if(dff<-5){
+			 Serial.println("逆");  
+			 Keyboard.println("B");
+				// Keyboard.println(KEY_LEFT_ARROW);  //左键       
 	  }
+	
 }
